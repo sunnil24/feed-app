b263cfdba7ee86a77d1327af35315a5252f48953
Sunil Srivastava <sunilsri.srivastava3@gmail.com>
2020-04-18 19:13:17 +0530

    Updated api endpoint

--- src/redux/actions/feed.js
+++ src/redux/actions/feed.js
@@ -1,42 +1,41 @@
 import {
+  FETCH_FEED_SUCCESS,
+  FETCH_FEED_FAILURE,
+  FETCH_FEED_INIT,
+} from "../types/feed";
 
- FETCH_FEED_SUCCESS,
- FETCH_FEED_FAILURE,
- FETCH_FEED_INIT,
-} from '../types/feed'
-
-import axios from 'axios';
+import axios from "axios";
 
 export const fetchFeed = (pageNumber) => (dispatch) => {
-  dispatch(fetchFeedInit())
+  dispatch(fetchFeedInit());
 
   axios
-      .get(`http://hn.algolia.com/api/v1/search?tags=story&page=${pageNumber}`,)
-      .then(res => {
-        dispatch(fetchFeedSuccess(res.data));
-      })
-      .catch(err => {
-        dispatch(fetchFeedError(err.message));
-      });
+    .get(`//hn.algolia.com/api/v1/search?tags=story&page=${pageNumber}`)
+    .then((res) => {
+      dispatch(fetchFeedSuccess(res.data));
+    })
+    .catch((err) => {
+      dispatch(fetchFeedError(err.message));
+    });
 };
 
-export const fetchFeedInit = data => ({
+export const fetchFeedInit = (data) => ({
   type: FETCH_FEED_INIT,
   payload: {
     data,
-  }
+  },
 });
 
-export const fetchFeedSuccess = data => ({
+export const fetchFeedSuccess = (data) => ({
   type: FETCH_FEED_SUCCESS,
   payload: {
     data,
-  }
+  },
 });
 
-export const fetchFeedError = data => ({
+export const fetchFeedError = (data) => ({
   type: FETCH_FEED_FAILURE,
   payload: {
     data,
-  }
+  },
 });
