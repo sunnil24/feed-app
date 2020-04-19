7ac4acc5c6d1ab634424e1e9798df0206a1c6203
Sunil Srivastava <sunilsri.srivastava3@gmail.com>
2020-04-19 18:03:10 +0530

    Added performance plugins

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 import React from "react";
 import asyncComponent from "./components/AsyncComponent";
+import Feed from "./containers/Feed";
 
 const AsyncFeed = asyncComponent(() => import("./containers/Feed"));
 
 export default [
   {
     path: "/",
-    component: <AsyncFeed />,
+    component: <Feed />,
   },
 ];
